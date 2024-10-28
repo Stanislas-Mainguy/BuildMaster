dbef04c5e16da987c44f04e62701a2f647392ee8
Stanislas-Mainguy <stan.mainguy@gmail.com>
2024-10-28 14:44:54 +0100

    Correction and deleted files

--- back-end/port.js
+++
@@ -1,6 +0,0 @@
-PORT=5000
-DB_USER=your_db_username
-DB_HOST=localhost
-DB_NAME=buildmaster_db
-DB_PASSWORD=your_db_password
-DB_PORT=5432
--- back-end/server.js
+++ back-end/server.js
@@ -10,12 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Route de base
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Bienvenue sur le serveur BuildMaster!');
 });
 
 // Endpoint de test pour vérifier la connexion à la base de données
-app.get('/db-test', async (req, res) => {
+app.get('/db-test', async (_, res) => {
     try {
         const result = await pool.query('SELECT NOW()');
         res.json(result.rows);
